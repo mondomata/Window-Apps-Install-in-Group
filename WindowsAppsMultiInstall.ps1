e6d129a96d9223be3110c6895b1ef7f2ed6734b0
mondomata <69905843+mondomata@users.noreply.github.com>
2025-01-19 18:09:35 +0100

    Update WindowsAppsMultiInstall.ps1

--- WindowsAppsMultiInstall.ps1
+++ WindowsAppsMultiInstall.ps1
@@ -1,11 +1,15 @@
 
-Write-Output "Push Y so yes, to run of script, if need to your machine. \n Nyomj Y-t, azaz yes/igen a szkript lefutásának engedélyezéséhez, ha kéri a géped."
+Write-Output "Push Y so yes, to run of script, if need to your machine." 
+Write-Output "Nyomj Y-t, azaz yes/igen a szkript lefutásának engedélyezéséhez, ha kéri a géped."
 Write-Output "********************************************************"
 Set-ExecutionPolicy -ExecutionPolicy RemoteSigned
-Write-Output "INSTALLING FAVORIT APPS IN GROUP \n FAVORIT APPOK INSTALLJA - CSOPORTOSAN"
-Write-Output "IF FOR YOU NEED CANCEL, AFTER RUN SCRIPT CAN UINSTALL \n HA MÉGSEM KELL, A SZKRIPT LEFUTÁSA UTÁN LEHET UNINSTALL."
-Write-Output "THE WORK MAYBE 2-3 MINUTES, DO NOT CLOSE THIS WINDOW. \n MOST A MUNKA KB. 2-3 PERCIG TART, NE ZÁRD BE EZT AZ ABLAKOT."
-Write-Output "___________________________"
+Write-Output "INSTALLING FAVORIT APPS IN GROUP"
+Write-Output "FAVORIT APPOK INSTALLJA - CSOPORTOSAN"
+Write-Output "IF FOR YOU NEED CANCEL, AFTER RUN SCRIPT CAN UINSTALL"
+Write-Output "HA MÉGSEM KELL, A SZKRIPT LEFUTÁSA UTÁN LEHET UNINSTALL."
+Write-Output "THE WORK MAYBE 2-3 MINUTES, DO NOT CLOSE THIS WINDOW. "
+Write-Output "MOST A MUNKA KB. 2-3 PERCIG TART, NE ZÁRD BE EZT AZ ABLAKOT."
+Write-Output "______________________________"
 Write-Output "Nyomj ENTERT a folytatáshoz..."
 PAUSE
 $apps = @(
