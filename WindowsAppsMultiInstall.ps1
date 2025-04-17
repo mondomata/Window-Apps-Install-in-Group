59c1c89b9de5c1ac98f77ac3c8fcd9ddf2c194db
mondomata <69905843+mondomata@users.noreply.github.com>
2025-04-17 14:38:54 +0200

    Update WindowsAppsMultiInstall.ps1

--- WindowsAppsMultiInstall.ps1
+++ WindowsAppsMultiInstall.ps1
@@ -1,19 +1,19 @@
 
-Write-Output "Push Y so yes, to run of script, if need to your machine." 
-Write-Output "Nyomj Y-t, azaz yes/igen a szkript lefutásának engedélyezéséhez, ha kéri a géped."
-Write-Output "********************************************************"
+Write-Output "Push Y so yes, to run of script, if need to your machine." ;
+Write-Output "Nyomj Y-t, azaz yes/igen a szkript lefutásának engedélyezéséhez, ha kéri a géped.";
+Write-Output "********************************************************";
 Set-ExecutionPolicy -ExecutionPolicy RemoteSigned
-Write-Output "INSTALLING FAVORIT APPS IN GROUP"
-Write-Output "FAVORIT APPOK INSTALLJA - CSOPORTOSAN"
-Write-Output "IF FOR YOU NEED CANCEL, AFTER RUN SCRIPT CAN UINSTALL"
-Write-Output "HA MÉGSEM KELL, A SZKRIPT LEFUTÁSA UTÁN LEHET UNINSTALL."
-Write-Output "THE WORK MAYBE 2-3 MINUTES, DO NOT CLOSE THIS WINDOW. "
-Write-Output "MOST A MUNKA KB. 2-3 PERCIG TART, NE ZÁRD BE EZT AZ ABLAKOT."
-Write-Output "______________________________"
-Write-Output "Nyomj ENTERT a folytatáshoz..."
+Write-Output "INSTALLING FAVORIT APPS IN GROUP";
+Write-Output "FAVORIT APPOK INSTALLJA - CSOPORTOSAN";
+Write-Output "IF FOR YOU NEED CANCEL, AFTER RUN SCRIPT CAN UINSTALL";
+Write-Output "HA MÉGSEM KELL, A SZKRIPT LEFUTÁSA UTÁN LEHET UNINSTALL.";
+Write-Output "THE WORK MAYBE 2-3 MINUTES, DO NOT CLOSE THIS WINDOW. ";
+Write-Output "MOST A MUNKA KB. 2-3 PERCIG TART, NE ZÁRD BE EZT AZ ABLAKOT.";
+Write-Output "______________________________";
+Write-Output "Nyomj ENTERT a folytatáshoz...";
 PAUSE
-Write-Output "START INSTALL PROGRAMMS... MOMENTS..."
-Write-Output "KEZDŐDIK A PROGRAMOK TELEPÍTÉSE... TÜRELEM..."
+Write-Output "START INSTALL PROGRAMMS... MOMENTS...";
+Write-Output "KEZDŐDIK A PROGRAMOK TELEPÍTÉSE... TÜRELEM...";
 $apps = @(
 
 "Brave.Brave" <# Brave browser böngésző (Chrome engine, but ad-free, secure, personal protect / Chrome motoros, de reklám mentes) #>
@@ -33,12 +33,12 @@ foreach ($app in $apps) {
     Winget Install $app
     }
 
-Write-Output "Plus gift - Godmode best extended Control panel made:"
-Write-Output "Plusz ajándék - Godmode, a legjobb kiterjesztett Vezérlőpult létrehozva:"
+Write-Output "Plus gift - Godmode best extended Control panel made:";
+Write-Output "Plusz ajándék - Godmode, a legjobb kiterjesztett Vezérlőpult létrehozva:";
 
 New-Item -Path '.\.{ED7BA470-8E54-465E-825C-99712043E01C}' -ItemType Directory
 
-Write-Output "EXTRA! EXTRA! EXTRA! Wait. No need? Close Windows/ Várj egy kicsit... Mutatok egy webes szkriptet, amivel rengeteg Windows beállítást tudsz elvégezni, nagyon egyszerűen. Million thank ChrisTitus.com. Nyomj Entert... Ha nem érdekel, zárd be az ablakot."
-Write-Output "Million thank ChrisTitus.com."
+Write-Output "EXTRA! EXTRA! EXTRA! Wait. No need? Close Windows/ Várj egy kicsit... Mutatok egy webes szkriptet, amivel rengeteg Windows beállítást tudsz elvégezni, nagyon egyszerűen. Million thank ChrisTitus.com. Nyomj Entert... Ha nem érdekel, zárd be az ablakot.";
+Write-Output "Million thank ChrisTitus.com.";
 pause
 iwr -useb https://christitus.com/win | iex
